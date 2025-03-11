135b4456dd14aa1b345691a377894448280dc66d
Ruslan Heorhiev <dev8.sofona@gmail.com>
2025-03-11 19:59:38 +0200

    add settings

--- app/public/http.php
+++ app/public/http.php
@@ -5,4 +5,5 @@ try {
 } catch (\Throwable $e) {
     $date = date('Y-m-d_H');
     file_put_contents("/var/www/runtime/logs/error-{$date}.log", print_r($e, 1) . PHP_EOL, FILE_APPEND);
+    print_r($e);
 }
